7b968d1866564f48a3699f1cba3e8822be6b00e3
Angshuman Barpujari <angshuman.barpujari@appyhigh.com>
2024-01-22 17:05:44 +0530

    remove listener

--- lib/src/internal/constants/constants.dart
+++ lib/src/internal/constants/constants.dart
@@ -1,6 +1,6 @@
 abstract class AdSdkConstants {
-  // static const baseUrl = "https://admob-automation-qa-cdn.apyhi.com";
-  static const baseUrl = "https://admob-automation.apyhi.com";
+  static const baseUrl = "https://admob-automation-qa-cdn.apyhi.com";
+  // static const baseUrl = "https://admob-automation.apyhi.com";
   static const endpoint = "/api/v2/app/info";
   static const infoTag = "AdSdk Info";
   static const errorTag = "AdSdk Error";
